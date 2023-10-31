89d82cdc6f04968caa4544e8f9fb9d127cadff9c
Nguyen Phuc Nguyen <npnguyen2k@gmail.com>
2023-10-31 15:48:09 +0700

    improve CreateSubscription method in service

--- internal/service/relationship/create_subscription.go
+++ internal/service/relationship/create_subscription.go
@@ -38,50 +38,41 @@ func (s service) CreateSubscription(ctx context.Context, createSubscriptionInput
 		return model.Relationship{}, err
 	}
 
-	if checkRelationship(relationships) == model.RelationshipTypeSubscribe {
+	rels := make(map[string]model.Relationship)
+	for _, r := range relationships {
+		rels[r.Type] = r
+	}
+
+	switch {
+	// subscription exists
+	case containsKey(rels, model.RelationshipTypeSubscribe.ToString()):
 		return model.Relationship{}, ErrSubscriptionExists
-	}
-
-	if checkRelationship(relationships) == model.RelationshipTypeBlock {
-		// find block
-		for _, r := range relationships {
-			if r.Type == model.RelationshipTypeBlock.ToString() {
-				r.Type = model.RelationshipTypeSubscribe.ToString()
-				subscription, err := s.relationshipRepo.Update(ctx, r)
-				if err != nil {
-					return model.Relationship{}, err
-				}
-				return subscription, nil
-			}
+	// block exists
+	case containsKey(rels, model.RelationshipTypeBlock.ToString()):
+		r := rels[model.RelationshipTypeBlock.ToString()]
+		r.Type = model.RelationshipTypeSubscribe.ToString()
+		subscription, err := s.relationshipRepo.Update(ctx, r)
+		if err != nil {
+			return model.Relationship{}, err
 		}
+		return subscription, nil
+	default:
+		// create a new subscription
+		subscription := model.Relationship{
+			RequestorID: requestorID,
+			TargetID:    targetID,
+			Type:        model.RelationshipTypeSubscribe.ToString(),
+		}
+		subscription, err = s.relationshipRepo.Create(ctx, subscription)
+		if err != nil {
+			return model.Relationship{}, err
+		}
+		return subscription, nil
 	}
 
-	// create subscription
-	subscription := model.Relationship{
-		RequestorID: requestorID,
-		TargetID:    targetID,
-		Type:        model.RelationshipTypeSubscribe.ToString(),
-	}
-
-	subscription, err = s.relationshipRepo.Create(ctx, subscription)
-	if err != nil {
-		return model.Relationship{}, err
-	}
-
-	return subscription, nil
 }
 
-func checkRelationship(relationships []model.Relationship) model.RelationshipType {
-	for _, r := range relationships {
-		// if subscription exists
-		if r.Type == model.RelationshipTypeSubscribe.ToString() {
-			return model.RelationshipTypeSubscribe
-		}
-
-		// if block exists
-		if r.Type == model.RelationshipTypeBlock.ToString() {
-			return model.RelationshipTypeBlock
-		}
-	}
-	return ""
+func containsKey(m map[string]model.Relationship, key string) bool {
+	_, exists := m[key]
+	return exists
 }
